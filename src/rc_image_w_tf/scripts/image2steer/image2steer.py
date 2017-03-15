c8aaf34f543eef8a53f8f246dc80f78e70337b6b
DaaasukeMurata <daaaaaasuke@gmail.com>
2017-03-15 19:47:04 +0900

    lineの傾きからsteer決める対応

--- src/rc_image_w_tf/scripts/image2steer/image2steer.py
+++ src/rc_image_w_tf/scripts/image2steer/image2steer.py
@@ -2,6 +2,7 @@
 # coding: UTF-8
 
 import os
+import math
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
 
@@ -32,6 +33,8 @@ class RcImageSteer():
         self.cnn.saver.restore(self.cnn.sess, CKPT_PATH + model_name)
 
         # --- for ROS ---
+        self.adj_steer = rospy.get_param("~line_adjust_steer", 20)
+
         self._cv_bridge = CvBridge()
         test_mode = rospy.get_param("~testmode", False)
         if test_mode:
@@ -40,23 +43,43 @@ class RcImageSteer():
             self._pub = rospy.Publisher('servo', UInt16MultiArray, queue_size=1)
         self._sub = rospy.Subscriber('image_processed', Image, self.callback, queue_size=1)
 
+    def steer_by_image(self, image):
+        x = np.reshape(image, (1, IMG_HEIGHT, IMG_WIDTH, IMG_DIM))
+        p = self.cnn.sess.run(self.cnn.predictions,
+                              feed_dict={self.cnn.input_holder: x,
+                                         self.cnn.keepprob_holder: 1.0})
+        answer = np.argmax(p, 1)
+        return answer[0]
+
+    def steer_by_line(self, x1, y1, x2, y2):
+        angle = math.atan2(y2 - y1, x2 - x1) / math.pi      # y2 > y1前提
+        dif = (angle - 0.5) * 10 * self.adj_steer
+        return 90 + dif
+
     def callback(self, image_msg):
         cv_image = self._cv_bridge.imgmsg_to_cv2(image_msg, "bgr8")
 
         # [height, width, depth]の形にして、dim3を削除
-        dim1, dim2, dim3 = np.dsplit(cv_image, 3)
-        image_array = np.dstack((dim1, dim2))
-        x = np.reshape(image_array, (1, IMG_HEIGHT, IMG_WIDTH, IMG_DIM))
+        dim1, dim2, lines = np.dsplit(cv_image, 3)
 
-        p = self.cnn.sess.run(self.cnn.predictions,
-                              feed_dict={self.cnn.input_holder: x,
-                                         self.cnn.keepprob_holder: 1.0})
+        line_trace = rospy.get_param("~use_line_trace", False)
+        if line_trace:
+            # lines[0, 0 〜 3, 0]:front line x1, y1, x2, y2
+            # lines[1, 0 〜 3, 0]:left  line x1, y1, x2, y2
+            f_x1, f_y1, f_x2, f_y2 = int(lines[0, 0, 0]), int(lines[0, 1, 0]), int(lines[0, 2, 0]), int(lines[0, 3, 0])
+            l_x1, l_y1, l_x2, l_y2 = int(lines[1, 0, 0]), int(lines[1, 1, 0]), int(lines[1, 2, 0]), int(lines[1, 3, 0])
 
-        answer = np.argmax(p, 1)
-        # print('answer %3d' % (answer))
+            if f_x1 != 0:
+                steer = self.steer_by_line(f_x1, f_y1, f_x2, f_y2)
+            else:
+                image_array = np.dstack((dim1, dim2))
+                steer = self.steer_by_image(image_array)
+        else:
+            image_array = np.dstack((dim1, dim2))
+            steer = self.steer_by_image(image_array)
 
         a = UInt16MultiArray()
-        a.data = [answer[0], 83]
+        a.data = [steer, 83]
         self._pub.publish(a)
 
     def main(self):
