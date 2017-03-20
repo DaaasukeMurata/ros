92775032a893057351610318b2441af014eacf67
DaaasukeMurata <daaaaaasuke@gmail.com>
2017-03-20 21:56:59 +0900

    model v3(line meta)対応

--- src/rc_image_w_tf/scripts/image2steer/image2steer.py
+++ src/rc_image_w_tf/scripts/image2steer/image2steer.py
@@ -22,6 +22,24 @@ IMG_WIDTH = 160
 IMG_DIM = 2
 
 
+class LineInfo(object):
+
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.piangle = self.__get_piangle()
+
+    def __get_piangle(self):
+        if self.x1 == 0:
+            return 0.
+        # y = tan(θ) * x + b
+        vy = int(self.y2) - int(self.y1)
+        vx = int(self.x2) - int(self.x1)
+        return math.atan2(vy, vx) / math.pi
+
+
 class RcImageSteer():
 
     def __init__(self):
@@ -44,51 +62,52 @@ class RcImageSteer():
         self._sub = rospy.Subscriber('image_processed', Image, self.callback, queue_size=1)
         print 'RcImageSteer init done.'
 
-    def steer_by_image(self, image):
-        x = np.reshape(image, (1, IMG_HEIGHT, IMG_WIDTH, IMG_DIM))
+    def steer_by_model(self, image, f_line, l_line):
+        img = image
+        line = np.array([f_line.x1, f_line.y1, f_line.x2, f_line.y2, f_line.piangle,
+                         l_line.x1, l_line.y1, l_line.x2, l_line.y2, l_line.piangle])
+        line = line.reshape([1, 10])
+
         p = self.cnn.sess.run(self.cnn.predictions,
-                              feed_dict={self.cnn.input_holder: x,
+                              feed_dict={self.cnn.image_holder: img,
+                                         self.cnn.line_meta_holder: line,
                                          self.cnn.keepprob_holder: 1.0})
         answer = np.argmax(p, 1)
         # print answer[0], p[0, answer[0]]
         return answer[0]
 
-    def steer_by_line(self, x1, y1, x2, y2):
-        # y2 > y1前提、視覚的にわかりやすいよう上下反転
-        angle = 1 + math.atan2(y1 - y2, x1 - x2) / math.pi
-        dif = (0.5 - angle) * 10 * self.adj_steer
-        # 線を乗り越えるとき、反転
-        if ((x1 + x2 < 255) and (angle < 0.5)) or ((x1 + x2 > 255) and (angle > 0.5)):
+    def steer_by_line(self, f_line):
+        if f_line.x1 == 0:
+            return None
+        dif = (0.5 - f_line.piangle) * 10 * self.adj_steer
+        if (((f_line.x1 + f_line.x2 < 255) and (f_line.piangle < 0.5)) or ((f_line.x1 + f_line.x2 > 255) and (f_line.piangle > 0.5))):
             dif = dif * -1
         steer = 90 + dif
         if steer < 30:
             steer = 30
         elif steer > 150:
             steer = 150
-        # print angle, steer
+        # print f_line.piangle, steer
         return steer
 
     def callback(self, image_msg):
         cv_image = self._cv_bridge.imgmsg_to_cv2(image_msg, "bgr8")
 
-        # [height, width, depth]の形にして、dim3を削除
+        # dim1とdim2が[height, width, depth]の画像、dim3はLineMetaInfo
         dim1, dim2, lines = np.dsplit(cv_image, 3)
+        image = np.dstack((dim1, dim2))
+        image = np.reshape(image, (1, IMG_HEIGHT, IMG_WIDTH, IMG_DIM))
+        f_line = LineInfo(int(lines[0, 0, 0]), int(lines[0, 1, 0]), int(lines[0, 2, 0]), int(lines[0, 3, 0]))
+        l_line = LineInfo(int(lines[1, 0, 0]), int(lines[1, 1, 0]), int(lines[1, 2, 0]), int(lines[1, 3, 0]))
+
+        steer = None
 
         line_trace = rospy.get_param("~use_line_trace", False)
         if line_trace:
-            # lines[0, 0 〜 3, 0]:front line x1, y1, x2, y2
-            # lines[1, 0 〜 3, 0]:left  line x1, y1, x2, y2
-            f_x1, f_y1, f_x2, f_y2 = int(lines[0, 0, 0]), int(lines[0, 1, 0]), int(lines[0, 2, 0]), int(lines[0, 3, 0])
-            l_x1, l_y1, l_x2, l_y2 = int(lines[1, 0, 0]), int(lines[1, 1, 0]), int(lines[1, 2, 0]), int(lines[1, 3, 0])
+            steer = self.steer_by_line(f_line)
 
-            if f_x1 != 0:
-                steer = self.steer_by_line(f_x1, f_y1, f_x2, f_y2)
-            else:
-                image_array = np.dstack((dim1, dim2))
-                steer = self.steer_by_image(image_array)
-        else:
-            image_array = np.dstack((dim1, dim2))
-            steer = self.steer_by_image(image_array)
+        if steer is None:
+            steer = self.steer_by_model(image, f_line, l_line)
 
         a = UInt16MultiArray()
         a.data = [steer, 83]
